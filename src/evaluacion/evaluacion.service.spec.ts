c0567a27a0192d88b093022089e3453331b47c41
Juan Esteban Lopez <j.lopezt@uniandes.edu.co>
2025-05-16 16:22:13 -0500

    Test Service de Evaluacion

--- src/evaluacion/evaluacion.service.spec.ts
+++ src/evaluacion/evaluacion.service.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EvaluacionService } from './evaluacion.service';
+import { Repository } from 'typeorm';
+import { EvaluacionEntity } from './evaluacion.entity';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('EvaluacionService', () => {
   let service: EvaluacionService;
+  let repository: Repository<EvaluacionEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [EvaluacionService],
     }).compile();
 
     service = module.get<EvaluacionService>(EvaluacionService);
+    repository = module.get<Repository<EvaluacionEntity>>(
+      getRepositoryToken(EvaluacionEntity),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create a evaluacion', async () => {
+    const evaluacion = new EvaluacionEntity();
+    evaluacion.calificacion = 5;
+
+    const createdEvaluacion = await service.create(evaluacion);
+    expect(createdEvaluacion).toBeDefined();
+    expect(createdEvaluacion.calificacion).toBe(evaluacion.calificacion);
+  });
+
+  it('should throw an error if evaluacion has invalid calificacion', async () => {
+    const evaluacion = new EvaluacionEntity();
+    evaluacion.calificacion = 0;
+
+    try {
+      await service.create(evaluacion);
+    } catch (error) {
+      expect(error).toBeDefined();
+      expect(error.message).toBe(
+        'La evaluación no cumple con los requisitos para ser creada',
+      );
+    }
+  });
+
 });
--- src/evaluacion/evaluacion.service.ts
+++ src/evaluacion/evaluacion.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { EvaluacionEntity } from './evaluacion.entity';
 import { Repository } from 'typeorm';
@@ -11,10 +11,10 @@ export class EvaluacionService {
   ) {}
 
   async create(evaluacion: EvaluacionEntity): Promise<EvaluacionEntity> {
-    if (evaluacion.calificacion < 5 && evaluacion.calificacion > 0) {
+    if (evaluacion.calificacion <= 5 && evaluacion.calificacion > 0) {
       return await this.evaluacionRepository.save(evaluacion);
     } else {
-      throw new Error(
+      throw new BadRequestException(
         'La evaluación no cumple con los requisitos para ser creada',
       );
     }
